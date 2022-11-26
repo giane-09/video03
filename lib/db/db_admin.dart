dbfa871553ff9e264f70a982a1b6113b2cab7d85
giane-09 <gianelaamv@gmail.com>
2022-11-25 21:06:27 -0500

    avancevideo09

--- lib/db/db_admin.dart
+++ lib/db/db_admin.dart
@@ -1,3 +1,98 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:video03/models/task_model.dart';
+
 class DBAdmin{
 
+  Database? myDatabase;
+
+  //Singleton
+  static final DBAdmin db = DBAdmin._();
+  DBAdmin._();
+  //
+
+  Future<Database?>checkDatabase()async {
+    if(myDatabase != null){
+      return myDatabase;
+    }
+    myDatabase = await initDatabase();// Crear base de datos
+    return myDatabase;
+  }
+
+  Future<Database?>initDatabase()async{
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, "TaskDB.db");
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db){},
+      onCreate: (Database dbx, int version) async{
+        //CREAR LA TABLA CORRESPONDIENTE
+        await dbx.execute("CREATE TABLE TASK(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, status TEXT)");
+      }
+    );
+    }
+    Future<int>insertRowTask(TaskModel model )async{
+    Database? db = await checkDatabase();
+    int res = await db!.rawInsert(
+        "INSERT INTO TASK(TITLE, DESCRIPTION, STATUS) VALUES ('${model.title}','${model.description}','${model.status.toString()}')");
+    return res;
+    }
+    Future<int>insertTask(TaskModel model)async {
+      Database? db = await checkDatabase();
+      int res = await db!.insert(
+        "task",
+        {
+          "title": model.title,
+          "description": model.description,
+          "status": model.status,
+        },
+      );
+      return res;
+    }
+
+    getRawTasks() async{
+    Database? db = await checkDatabase();
+    List tasks = await db!.rawQuery("SELECT * FROM task");
+    print(tasks[0]);
+    }
+    Future<List<TaskModel>>getTasks() async{
+    Database? db = await checkDatabase();
+    List<Map<String, dynamic>> tasks = await db!.query("Task");
+    List<TaskModel> taskModelList = tasks.map((e) => TaskModel.deMapAModel(e)).toList();
+
+    // tasks.forEach((element){
+    //   TaskModel task = TaskModel.deMapAModel(element);
+    //   taskModelList.add(task);
+    // });
+    return taskModelList;
+    }
+    updateRawTask() async{
+    Database? db = await checkDatabase();
+    int res = await db!.rawUpdate("CREATE TASK SET title = 'Ir de compras', description ='Comprar comida',status='true' WHERE id = 2");
+    print(res);
+    }
+    updateTask()async{
+    Database? db = await checkDatabase();
+    int res = await db!.update(
+      "TASK",
+      {
+        "title": "ir al cine","description": "Es el viernes en la tarde", "status":"false",
+      },
+      where: "id = 2"
+    );
+    }
+    deleteRawTask()async{
+    Database? db = await checkDatabase();
+    int res = await db!.rawDelete("DELETE FROM TASK WHERE id = 2 ");
+    print(res);
+
+    }
+    deleteTask()async{
+    Database? db = await checkDatabase();
+    int res = await db!.delete("task", where: "id =3");
+    print(res);
+    }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+import 'package:video03/db/db_admin.dart';
+import 'package:video03/models/task_model.dart';
+import 'package:video03/widgets/my_form_widget.dart';
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Future<String> getFullName() async{
+    return "Juan Manuel";
+  }
+
+  showDialogForm(){
+      showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return MyFormWidget();
+        },
+          );
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    DBAdmin.db.getTasks();
     return Scaffold(
       appBar: AppBar(
         title: Text("HomePage"),
+     ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialogForm();
+        },
+        child: const Icon(Icons.add),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: (){}, child: Text("Mostrar data"),),
-        ],
-      ),
+      body: FutureBuilder(
+        future: DBAdmin.db.getTasks(),
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.hasData) {
+            List<TaskModel>  myTasks = snap.data;
+            return ListView.builder(
+              itemCount: myTasks.length,
+                itemBuilder: (BuildContext context,int index){
+                return ListTile(
+                  title: Text(myTasks[index].title),
+                  subtitle: Text(myTasks[index].description),
+                  trailing: Text(myTasks[index].id.toString()),
+                );
+                },
+            );//       );
+          }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+        },
+      )
+
     );
   }
 }
